8493636ddc0a73670afab6e55832d1c7e398f217
balao1312 <62583750+balao1312@users.noreply.github.com>
2020-06-08 17:50:59 +0800

    v1

--- combine.py
+++ combine.py
@@ -19,4 +19,12 @@ df_temp = pandas.merge(df_temp, df_HR, on=['year','mlb_id', 'pitch_type'], how='
 df_temp = df_temp.fillna(0)
 df_temp['BA'] = (df_temp['1B_times'] + df_temp['2B_times'] + df_temp['3B_times'] +
                  df_temp['HR_times']) / df_temp['times']
+
+df_temp['BA_weight'] = (df_temp['1B_times']*0.89 + df_temp['2B_times']*1.27 +
+                        df_temp['3B_times']*1.62 +
+                 df_temp['HR_times']*2.1) / df_temp['times']
+
+afilter = df_temp[(df_temp['year'] == 2008) | (df_temp['year'] == 2009) ].index
+
+df_temp.drop(afilter,inplace=True)
 df_temp.to_csv('statistics.csv',encoding='utf8')
--- to_mysql.py
+++ to_mysql.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-host= ''
-port=
-user= ''
+host= '1.tcp.jp.ngrok.io'
+port= 23879
+user= 'balao1312'
 password= ''
 db = 'MoneyBallDatabase'
 
@@ -12,7 +12,7 @@ engine = create_engine(f'mysql+mysqldb://{user}:{password}@{host}:{port}/{db}')
 
 try:
     df = pd.read_csv('statistics.csv', encoding='utf8')
-    df.to_sql('statistics',engine)
+    #df.to_sql('statistics',engine)
     print("Write to MySQL successfully!")
 except Exception as e:
     print(e)
